6da0a91c02544a33a5860467a460bf19a6c38cce
YoungChulShin <go1323@gmail.com>
2021-12-08 06:41:03 +0900

    프로젝트 초기 소스

---
+++ kafka-spring-consumer/src/main/java/study/spring/kafka/kafkaspringconsumer/KafkaSpringConsumerApplication.java
@@ -0,0 +1,13 @@
+package study.spring.kafka.kafkaspringconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaSpringConsumerApplication.class, args);
+  }
+
+}
