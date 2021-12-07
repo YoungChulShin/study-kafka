3e7d3558ef1f7dcebbce873519c55546a959feba
YoungChulShin <go1323@gmail.com>
2021-12-07 23:14:34 +0900

    custom template 추가

---
+++ kafka-spring-producer/src/main/java/study/spring/kafka/kafkaspringproducer/KafkaTemplateConfiguration.java
@@ -0,0 +1,26 @@
+package study.spring.kafka.kafkaspringproducer;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+public class KafkaTemplateConfiguration {
+
+  @Bean
+  public KafkaTemplate<String, String> customKafkaTemplate() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+    DefaultKafkaProducerFactory<String, String> pf = new DefaultKafkaProducerFactory<>(props);
+
+    return new KafkaTemplate<>(pf);
+  }
+}
--- kafka-spring-producer/src/main/java/study/spring/kafka/kafkaspringproducer/SpringProducerApplication.java
+++ kafka-spring-producer/src/main/java/study/spring/kafka/kafkaspringproducer/SpringProducerApplication.java
@@ -1,21 +1,39 @@
 package study.spring.kafka.kafkaspringproducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 public class SpringProducerApplication implements CommandLineRunner {
 
   private static final String TOPIC_NAME = "test";
 
   @Autowired
-  private KafkaTemplate<Integer, String> template;
+  @Qualifier(value = "customKafkaTemplate")
+  private KafkaTemplate<String, String> template;
 
   @Override
   public void run(String... args) {
-    for (int i = 0; i < 10; i++) {
-      template.send(TOPIC_NAME, "test" + i);
-    }
+    ListenableFuture<SendResult<String, String>> future = template.send(TOPIC_NAME, "test");
+    future.addCallback(new KafkaSendCallback<String, String>() {
+
+      @Override
+      public void onFailure(KafkaProducerException ex) {
+
+      }
+
+      @Override
+      public void onSuccess(SendResult<String, String> result) {
+
+      }
+    });
+
     System.exit(0);
   }
 }
