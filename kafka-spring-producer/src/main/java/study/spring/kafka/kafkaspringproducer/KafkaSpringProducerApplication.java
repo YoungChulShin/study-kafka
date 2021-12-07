85af01d78b19b70b19ce072fe3c8da9b77d6bb02
YoungChulShin <go1323@gmail.com>
2021-12-07 22:43:12 +0900

    프로젝트 초기 소스

---
+++ kafka-spring-producer/src/main/java/study/spring/kafka/kafkaspringproducer/KafkaSpringProducerApplication.java
@@ -0,0 +1,13 @@
+package study.spring.kafka.kafkaspringproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringProducerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaSpringProducerApplication.class, args);
+  }
+
+}
---
+++ kafka-spring-producer/src/test/java/study/spring/kafka/kafkaspringproducer/KafkaSpringProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.kafka.kafkaspringproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringProducerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
