f310e310f503947da533642c812c6f0d518f7555
YoungChulShin <go1323@gmail.com>
2022-08-05 06:46:34 +0900

    프로젝트 초안

---
+++ kafka-spring-producer2/src/test/java/study/kafka/kafkaspringproducer2/KafkaSpringProducer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kafka.kafkaspringproducer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringProducer2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
