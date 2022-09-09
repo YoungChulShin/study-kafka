72e7ca6734ee3168bbde78443d0efef53b867d9e
YoungChulShin <go1323@gmail.com>
2022-09-09 21:56:05 +0900

    프로젝트 스캐폴드

---
+++ project-order-system/src/main/kotlin/study/kafka/order/projectordersystem/ProjectOrderSystemApplication.kt
@@ -0,0 +1,11 @@
+package study.kafka.order.projectordersystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectOrderSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectOrderSystemApplication>(*args)
+}
