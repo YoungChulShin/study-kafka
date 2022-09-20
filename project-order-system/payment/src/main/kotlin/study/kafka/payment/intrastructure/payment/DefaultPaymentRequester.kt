a16ba79980980ce12a3a7ca7c5312b6e38a88614
YoungChulShin <go1323@gmail.com>
2022-09-20 23:49:53 +0900

    결제 실패 케이스 추가 - 100원 미만은 실패

--- project-order-system/payment/src/main/kotlin/study/kafka/payment/intrastructure/payment/DefaultPaymentRequester.kt
+++ project-order-system/payment/src/main/kotlin/study/kafka/payment/intrastructure/payment/DefaultPaymentRequester.kt
@@ -6,7 +6,9 @@ import study.kafka.payment.domain.PaymentRequester
 @Component
 class DefaultPaymentRequester: PaymentRequester {
 
+    // 100원 미만은 소액이라서 실패하도록 설정한다
     override fun request(price: Int): Boolean {
-        return true
+        Thread.sleep(1000)
+        return price >= 100
     }
 }
