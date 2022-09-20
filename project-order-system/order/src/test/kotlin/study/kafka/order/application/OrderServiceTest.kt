8e44a31a502993fe27aad7b6a3a9a90e4c64c29e
YoungChulShin <go1323@gmail.com>
2022-09-21 08:43:02 +0900

    결제 결과 테스트 코드 추가

--- project-order-system/order/src/test/kotlin/study/kafka/order/application/OrderServiceTest.kt
+++ project-order-system/order/src/test/kotlin/study/kafka/order/application/OrderServiceTest.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import study.kafka.order.domain.Order
 import study.kafka.order.domain.OrderRepository
+import study.kafka.order.domain.OrderStatus
+import java.util.*
 
 class OrderServiceTest {
 
@@ -23,5 +25,41 @@ class OrderServiceTest {
 
         // then
         Assertions.assertThat(eventPublisher.events.size).isEqualTo(1)
+    }
+
+    @Test
+    fun if_payment_success_update_order_status_to_completed() {
+        // given
+        val orderRepositoryMock = Mockito.mock(OrderRepository::class.java)
+        val eventPublisher = TestEventPublisher()
+        val sut = OrderService(orderRepositoryMock, eventPublisher)
+
+        val orderId = 1L
+        val order = Order("삼각김밥", 1000)
+        Mockito.`when`(orderRepositoryMock.findById(orderId)).thenReturn(Optional.of(order))
+
+        // when
+        sut.updatePaymentResult(orderId, true)
+
+        // then
+        Assertions.assertThat(order.status).isEqualTo(OrderStatus.PAYMENT_COMPLETED)
+    }
+
+    @Test
+    fun if_payment_fail_update_order_status_to_cancelled() {
+        // given
+        val orderRepositoryMock = Mockito.mock(OrderRepository::class.java)
+        val eventPublisher = TestEventPublisher()
+        val sut = OrderService(orderRepositoryMock, eventPublisher)
+
+        val orderId = 1L
+        val order = Order("삼각김밥", 1000)
+        Mockito.`when`(orderRepositoryMock.findById(orderId)).thenReturn(Optional.of(order))
+
+        // when
+        sut.updatePaymentResult(orderId, false)
+
+        // then
+        Assertions.assertThat(order.status).isEqualTo(OrderStatus.CANCELLED)
     }
 }
