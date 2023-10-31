67d3f8fc520c50576e5510fd250a43373b95a131
YoungChulShin <go1323@gmail.com>
2023-10-31 20:30:37 +0900

    01. 대량 트래픽 처리 - jmeter 테스트

--- 01.large-traffic-handle-test/src/main/java/study/kafka/largetraffichandle/application/service/TestService.java
+++ 01.large-traffic-handle-test/src/main/java/study/kafka/largetraffichandle/application/service/TestService.java
@@ -1,13 +1,17 @@
 package study.kafka.largetraffichandle.application.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import study.kafka.largetraffichandle.application.port.in.TestUseCase;
 
+@Slf4j
 @Component
 class TestService implements TestUseCase {
 
   @Override
   public void calculate() {
+    log.info("계산을 시작합니다.");
     for (long i = 0; i <= 3000000000L; i++) { }
+    log.info("계산을 완료했습니다.");
   }
 }
