0a96557e633202c4a2a92670bed54a74127e9a89
YoungChulShin <go1323@gmail.com>
2023-10-31 16:48:58 +0900

    01. 대량 트래픽 처리 - 프로젝트 스케폴드

---
+++ 01.large-traffic-handle-test/src/main/java/study/kafka/largetraffichandle/LargeTrafficHandleApplication.java
@@ -0,0 +1,13 @@
+package study.kafka.largetraffichandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeTrafficHandleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LargeTrafficHandleApplication.class, args);
+  }
+
+}
---
+++ 01.large-traffic-handle-test/src/test/java/study/kafka/largetraffichandle/LargeTrafficHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kafka.largetraffichandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeTrafficHandleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
